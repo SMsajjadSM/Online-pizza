ef94918aa9e87a6c0b493d7d636637c43bc5a7ce
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-06-06 12:26:24 +0330

    new file:   src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import CreateOrder from "./features/order/CreateOrder"
+import Order from "./features/order/Order"
+function App() {
+  const router = createBrowserRouter([
+    {path:'/',element:<Home/>},
+    {path:'/menu' , element:<Menu/>},
+    {path:"/order/new" , element:<CreateOrder/>},
+    {path:"/order/order:Id", element:<Order/>}
+  ]) 
+  return (
+ <RouterProvider router={router}/>
+  )
+}
+
+export default App
