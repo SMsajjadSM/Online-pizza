6f09035b9befec448fa2064481c99439903de908
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-06-11 18:27:40 +0330

    src/features/order/CreateOrder.jsx

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -31,6 +33,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -38,7 +43,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -48,6 +53,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+          {formErrors?.phone && <p>${formErrors.phone}</p>}   
           </div>
         </div>
 
@@ -70,11 +76,32 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(fakeCart)} />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? `placing order ... ` : `Order now`}
+          </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+ 
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+  const errors = {};
 
+  if (!isValidPhone(Number(order.phone)))
+    errors.phone = "plz insert correct number for contact to you";
+
+
+  if (Object.keys(errors).length > 0) return errors;
+  const neworder = await createOrder(order);
+  return redirect(`/order/${neworder.id}`);
+}
 export default CreateOrder;
