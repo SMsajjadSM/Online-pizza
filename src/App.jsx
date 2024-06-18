255583f94658b0e0a67a82536cf2ce9580b3970b
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-06-18 08:12:27 +0330

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu,{loader as loaderMenu} from "./features/menu/Menu";
-import CreateOrder,{action as createOrderAction} from "./features/order/CreateOrder";
-import Order,{loader as loaderOrder} from "./features/order/Order";
+import Menu, { loader as loaderMenu } from "./features/menu/Menu";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as loaderOrder } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Cart from "./features/cart/Cart";
-import Error from "./ui/Error"
+import Error from "./ui/Error";
 function App() {
   const router = createBrowserRouter([
     {
       element: <AppLayout />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "/menu", element: <Menu />,      errorntEleme:<Error/>,
-          loader: loaderMenu},
-        { path: "/order/new", element: <CreateOrder />,action:createOrderAction },
-        { path: "/order/:orderId", element: <Order />, loader:loaderOrder,errorntEleme:<Error/>},
+        {
+          path: "/menu",
+          element: <Menu />,
+          errorntEleme: <Error />,
+          loader: loaderMenu,
+        },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+          action: createOrderAction,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+          loader: loaderOrder,
+          errorntEleme: <Error />,
+        },
         { path: "/cart", element: <Cart /> },
       ],
     },
