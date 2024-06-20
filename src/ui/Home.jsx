40f44541a8bfc6366d7ab8fbeae8cffd21cc646e
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-06-20 22:18:44 +0330

    new file:   src/ui/Home.jsx

---
+++ src/ui/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="font-semibold text-center text-xl">
+        The best pizza.
+        <br />
+        <span className="text-yellow-500">
+        Straight out of the oven, straight to you.
+        </span>
+
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
