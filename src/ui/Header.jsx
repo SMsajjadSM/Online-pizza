1622c51049d4a05e56fb54094b187c328da74e07
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-06-25 18:36:45 +0330

    src/ui/Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../features/order/SearchOrder"
-
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 function Header() {
-    return (
-        <header className="bg-yellow-500 uppercase">
-            <Link className="tracking-widest" to="/">Online Pizza</Link>
-            <SearchOrder/>
-            <p>SMsajjadSM</p>
-        </header>
-    )
+  return (
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3">
+      <Link className="tracking-widest" to="/">
+        Online Pizza
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
