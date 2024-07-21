ff090bfce7c6fa9332b630994099de2caaf11767
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-07-21 23:18:00 +0330

    src/features/menu/MenuItem.jsx

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,20 +1,23 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+function handleAddToCrt(){
+  console.log(id)
 
+}
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(', ')}
+          {ingredients.join(", ")}
         </p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
@@ -24,8 +27,7 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-
-          <Button type="small">Add to cart</Button>
+          { !soldOut && <Button onClick={handleAddToCrt} type="small">Add to cart</Button>}
         </div>
       </div>
     </li>
