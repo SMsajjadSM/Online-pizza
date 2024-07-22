66cb5eb8d9b754e6f2f0527c9f2466190787517d
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-07-22 19:00:53 +0330

    src/features/user/Username.jsx

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">SMsajjadSM</div>;
+  const userName = useSelector((state)=> state.user.userName)
+  return <div className="hidden text-sm font-semibold md:block">{userName}</div>;
 }
 
 export default Username;
