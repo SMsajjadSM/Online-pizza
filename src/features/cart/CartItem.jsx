b8b89d41170f76e3865e656909ac7288b605d167
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-08-05 20:34:48 +0330

    src/features/cart/CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,17 +1,26 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
-
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
+import { currentQuentityById, deleteItem } from "./cartSlice";
+import UpdateItemQuentity from "./UpdateItemQuentity";
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
-
+  const { pizzaId, name, quentity, totalPrice } = item;
+  const dispatch = useDispatch();
+  const currentQuentity = useSelector(currentQuentityById(pizzaId));
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
-        {quantity}&times; {name}
+        {quentity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <UpdateItemQuentity
+          currentQuentity={currentQuentity}
+          pizzaId={pizzaId}
+        />
+        <Button onClick={() => dispatch(deleteItem(pizzaId))} type="small">
+          Delete
+        </Button>
       </div>
     </li>
   );
