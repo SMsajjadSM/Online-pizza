a71a9faf3d6d23b7d0125f9a92d27897e3cb43be
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-08-06 18:29:13 +0330

    src/features/cart/CartOverview.jsx

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getTotalCartQuentity,getTotalCarPrice } from "./cartSlice";
+import { getTotalCartQuentity, getTotalCarPrice } from "./cartSlice";
 import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuentity);
-  const totalPrice = useSelector(getTotalCarPrice)
-  console.log(totalCartQuantity);
-  if(!totalPrice)return null
+  const totalPrice = useSelector(getTotalCarPrice);
+  if (!totalPrice) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
