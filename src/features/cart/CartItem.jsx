dc60815d7c8a6489489ed22970680fe555ceb24f
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-08-12 19:07:23 +0330

    src/features/cart/CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,26 +1,27 @@
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../../ui/Button";
-import { formatCurrency } from "../../utils/helpers";
-import { currentQuentityById, deleteItem } from "./cartSlice";
-import UpdateItemQuentity from "./UpdateItemQuentity";
+import { useSelector } from 'react-redux';
+import { formatCurrency } from '../../utils/helpers';
+import DeleteItem from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from './cartSlice';
+
 function CartItem({ item }) {
-  const { pizzaId, name, quentity, totalPrice } = item;
-  const dispatch = useDispatch();
-  const currentQuentity = useSelector(currentQuentityById(pizzaId));
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
-        {quentity}&times; {name}
+        {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <UpdateItemQuentity
-          currentQuentity={currentQuentity}
+
+        <UpdateItemQuantity
           pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
         />
-        <Button onClick={() => dispatch(deleteItem(pizzaId))} type="small">
-          Delete
-        </Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
