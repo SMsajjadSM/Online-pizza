877c76ae90f10a5c740233247c8e5074e0870793
SMsajjadSM <sajjadsiadati1390@gmail.com>
2024-08-24 23:30:07 +0330

    src/features/menu/MenuItem.jsx

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,34 +1,40 @@
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../../ui/Button";
-import { formatCurrency } from "../../utils/helpers";
-import { addItem, currentQuentityById, deleteItem } from "../cart/cartSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../../ui/Button';
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
+import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-  const currentQuentity = useSelector(currentQuentityById(id));
-  const isInCart = currentQuentity > 0;
-  function handleAddToCrt() {
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
+  function handleAddToCart() {
     const newItem = {
       pizzaId: id,
       name,
-      quentity: 1,
+      quantity: 1,
       unitPrice,
       totalPrice: unitPrice * 1,
     };
     dispatch(addItem(newItem));
   }
+
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(", ")}
+          {ingredients.join(', ')}
         </p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
@@ -38,13 +44,19 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+
           {isInCart && (
-            <Button onClick={() => dispatch(deleteItem(id))} type="small">
-              Delete
-            </Button>
-          )}{" "}
-          {!soldOut && !isInCart &&(
-            <Button onClick={handleAddToCrt} type="small">
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+
+          {!soldOut && !isInCart && (
+            <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
           )}
